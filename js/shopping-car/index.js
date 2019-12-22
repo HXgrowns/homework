2f5fdb9a765eba3f891912a0a5be0420fa56888e
huxiao <804156017@qq.com>
2019-12-22 22:21:16 +0800

    [huxiao] {refactor}: refactor shopping-car

--- js/shopping-car/index.js
+++ js/shopping-car/index.js
@@ -1,125 +1,107 @@
 const select = document.getElementById('goods-list');
 const table = document.getElementById('table');
 
+var data = carProducts;
+
 window.onload = function () {
-  for (let i of carProducts) {
-    var option = document.createElement('option');
-    option.value = i.id;
-    option.innerText = i.name;
-    select.appendChild(option);
+  var option = "<option value='-1'>==请选择商品==</option>";
+  for (let i of data) {
+    option += `<option value="${i.id}">${i.name}</option>`;
+  }
+  select.innerHTML = option;
+}
+
+function shoppingGoods(goods) {
+  var tr = table.insertRow(table.rows.length - 1);
+  var row = ` 
+  <tr> 
+    <td>
+      <input type="checkbox" id="checkBox-${goods.id}" onclick="selectedRow(${goods.id})"/>
+    </td>
+    <td>${goods.name}</td>
+    <td id="price-${goods.id}">${goods.price}</td>
+    <td>
+      <button id="substract${goods.id}" class="button" onclick="substract(${goods.id})">-</button>
+      <span id="count-${goods.id}" class="count">${goods.count}</span>
+      <button id="add${goods.id}" class="button" onclick="add(${goods.id})">+</button>
+    </td>
+    <td><span id="total-${goods.id}">${goods.price * goods.count}</span></td>
+  </tr>
+  `;
+  tr.id = "row-" + goods.id;
+  tr.innerHTML += row;
+}
+
+function selectedDroplist() {
+  var goodsId = select.value;
+  var goods = data[goodsId];
+  if (goods.count <= 1) {
+    goods.count = 1;
+    shoppingGoods(goods);
+  } else {
+    var goodsNum = document.getElementById('count-' + goodsId);
+    goods.count++;
+    goodsNum.innerText = goods.count;
+  }
+};
+
+function selectedRow(targetId) {
+  var checkBox = document.getElementById('checkBox-' + targetId);
+  var totalSumPrice = document.getElementById('totalSumPrice');
+  var totalSumNum = document.getElementById('totalSumNum');
+  var totalPrice = document.getElementById('total-' + targetId);
+  var goodsNum = document.getElementById('count-' + targetId);
+  if (checkBox.checked) {
+    totalSumNum.innerText = parseFloat(totalSumNum.innerText) + parseFloat(goodsNum.innerText);
+    totalSumPrice.innerText = parseFloat(totalSumPrice.innerText) + parseFloat(totalPrice.innerText);
+  } else {
+    totalSumNum.innerText = parseFloat(totalSumNum.innerText) - parseFloat(goodsNum.innerText);
+    totalSumPrice.innerText = parseFloat(totalSumPrice.innerText) - parseFloat(totalPrice.innerText);
   }
 }
 
-select.onchange = function () {
-  var goodsId = select.value;
-  var goods = carProducts[goodsId - 1];
-  shoppingGoods(goods);
-};
-
-table.addEventListener("click", function (e) {
-  if (e.target) {
-    var targetStr = e.target.id
-    var targetId = targetStr.charAt(targetStr.length - 1);
-    var goodsNum = document.getElementById('count-' + targetId);
-    var total = document.getElementById('total-' + targetId);
-
-    if (e.target.nodeName.toLowerCase() == "button") {
-      var targetType = targetStr.substring(0, targetStr.length - 2);
-      if (targetType === 'subtract') {
-        if (goodsNum.innerText == 1) {
-          var targetRow = document.getElementById('row-' + targetId);
-          table.removeChild(targetRow);
-        }
-        goodsNum.innerText--;
-      } else if (targetType === 'add') {
-        goodsNum.innerText++;
+function selectAll() {
+  var checkBoxAll = document.getElementById('checkBox-' + 0);
+  var allChild = table.childNodes[1].childNodes;
+  for (let index = 4, len = allChild.length; index < len - 2; index++) {
+    var row = allChild[index];
+    var rowId = row.id.charAt(row.id.length - 1);
+    var checkBox = document.getElementById('checkBox-' + rowId);
+    if (checkBoxAll.checked) {
+      if (!checkBox.checked) {
+        checkBox.checked = true;
+        selectedRow(rowId);
       }
-      var price = document.getElementById('price-' + targetId);
-      total.innerText = price.innerText * goodsNum.innerText;
-    } else if (e.target.nodeName.toLowerCase() == "input") {
-      var checkBox = document.getElementById('checkBox-' + targetId);
-      var totalSumNum = document.getElementById('totalSumNum');
-      var totalSumPrice = document.getElementById('totalSumPrice');
-      if (targetId === '0') {
-        var allChild = table.childNodes[1].childNodes;
-        for (let index = 2, len = allChild.length; index < len - 2; index++) {
-          var row = allChild[index];
-          var rowId = row.id.charAt(row.id.length - 1);
-          var checkBoxChild = document.getElementById('checkBox-' + rowId);
-          var totalSumNum = document.getElementById('totalSumNum');
-          var totalSumPrice = document.getElementById('totalSumPrice');
-          if (checkBox.checked) {
-            if (!checkBoxChild.checked) {
-              checkBoxChild.checked = true;
-              var goodsNum = document.getElementById('count-' + rowId);
-              var total = document.getElementById('total-' + rowId);
-              totalSumNum.innerText = parseInt(totalSumNum.innerText) + parseInt(goodsNum.innerText);
-              totalSumPrice.innerText = parseInt(totalSumPrice.innerText) + parseInt(total.innerText);
-            }
-          } else {
-            checkBoxChild.checked = false;
-            totalSumNum.innerText = 0;
-            totalSumPrice.innerText = 0;
-          }
-        }
-      } else {
-        if (checkBox.checked) {
-          totalSumNum.innerText = parseInt(totalSumNum.innerText) + parseInt(goodsNum.innerText);
-          totalSumPrice.innerText = parseInt(totalSumPrice.innerText) + parseInt(total.innerText);
-        } else {
-          totalSumNum.innerText = parseInt(totalSumNum.innerText) - parseInt(goodsNum.innerText);
-          totalSumPrice.innerText = parseInt(totalSumPrice.innerText) - parseInt(total.innerText);
-        }
+    } else {
+      if (checkBox.checked) {
+        checkBox.checked = false;
+        selectedRow(rowId);
       }
     }
   }
-});
+}
 
-function shoppingGoods(goods) {
-  var table = document.getElementById('table');
-  var tr = document.createElement('tr');
 
-  var tdSelect = document.createElement('td');
-  var checkBox = document.createElement('input');
-  checkBox.id = 'checkBox-' + goods.id;
-  checkBox.type = 'checkbox';
-  tdSelect.appendChild(checkBox);
+function substract(targetId) {
+  var goodsNum = document.getElementById('count-' + targetId);
+  if (goodsNum.innerText === '1') {
+    var targetRow = document.getElementById('row-' + targetId);
+    var tbody = targetRow.parentNode;
+    tbody.removeChild(targetRow);
+  }
+  data[targetId].count--;
+  goodsNum.innerText = data[targetId].count;
+  everSum(targetId);
+}
 
-  var tdName = document.createElement('td');
-  tdName.innerText = goods.name;
+function add(targetId) {
+  var goodsNum = document.getElementById('count-' + targetId);
+  data[targetId].count++;
+  goodsNum.innerText = data[targetId].count;
+  everSum(targetId);
+}
 
-  var tdPrice = document.createElement('td');
-  tdPrice.id = 'price-' + goods.id;
-  tdPrice.innerText = goods.price;
-
-  var tdNum = document.createElement('td');
-  var subtract = document.createElement('button');
-  subtract.id = 'subtract-' + goods.id;
-  subtract.innerText = '-';
-  subtract.className = 'button';
-  var num = document.createElement('span');
-  num.id = 'count-' + goods.id;
-  num.innerText = goods.count;
-  num.className = 'count';
-  var add = document.createElement('button');
-  add.id = 'add-' + goods.id;
-  add.innerText = '+';
-  add.className = 'button';
-  tdNum.appendChild(subtract);
-  tdNum.appendChild(num);
-  tdNum.appendChild(add);
-
-  var tdTotal = document.createElement('td');
-  tdTotal.id = "total-" + goods.id;
-  tdTotal.innerText = goods.price * goods.count;
-
-  var allChild = table.childNodes;
-  len = allChild.length
-  tr = table.insertRow(len - 1);  
-  tr.id = 'row-' + goods.id;
-  tr.appendChild(tdSelect);
-  tr.appendChild(tdName);
-  tr.appendChild(tdPrice);
-  tr.appendChild(tdNum);
-  tr.appendChild(tdTotal);
+function everSum(targetId) {
+  var sumPrice = document.getElementById('total-' + targetId);
+  sumPrice.innerText = data[targetId].count * data[targetId].price;
 }
